f1c10a20b652c95f3814ee22690379d284e31b22
Duong Hong An <themasterclaus@gmail.com>
2022-06-13 16:26:50 +0700

    importer manifest & shadow

--- slimeworldmanager-importer/build.gradle.kts
+++ slimeworldmanager-importer/build.gradle.kts
@@ -1,3 +1,12 @@
+plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+application {
+    mainClass.set("com.grinderwolf.swm.importer.SWMImporter")
+}
+
 dependencies {
     implementation(project(":slimeworldmanager-api"))
     implementation(project(":slimeworldmanager-nms-common"))
@@ -6,6 +15,12 @@ dependencies {
     implementation("com.github.luben:zstd-jni:1.5.2-2")
     implementation("com.github.tomas-langer:chalk:1.0.2")
     implementation("com.flowpowered:flow-nbt:2.0.2")
+}
+
+tasks {
+    assemble {
+        dependsOn(shadowJar)
+    }
 }
 
 description = "slimeworldmanager-importer"
